a4b7604728f278421081bcfc77f664427b0b4b84
Binsadboiiz <ngnphcng@gmail.com>
2025-02-08 15:08:34 +0700

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import '../css/header.css'; // Import CSS file for styling
+import React from "react";
+import { useState } from "react";
+import '../css/header.css';
+import LogInForm from "./form";
 
-const Header = () => {
-  return (
-    <header className="header">
-      <h1>My App</h1>
-      <nav>
-        <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
-    </header>
-  );
+export default function Header() {
+    const [showLogInForm, setShowLogInForm] = useState(false);
+    return (
+        <div>
+            <header>
+                <h1 className="logo">Zara Therapy Clinic</h1>
+                <ul className="listnav">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Products</a>
+                        <ul className="productbars">
+                            <li><a href="#">Product1</a></li>
+                            <li><a href="#">Product1</a></li>
+                            <li><a href="#">Product1</a></li>
+                            <li><a href="#">Product1</a></li>
+                            <li><a href="#">Product1</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Services</a>
+                        <ul className="servicebars">
+                            <li><a href="#">Service1</a></li>
+                            <li><a href="#">Service1</a></li>
+                            <li><a href="#">Service1</a></li>
+                            <li><a href="#">Service1</a></li>
+                            <li><a href="#">Service1</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Home</a></li>
+                </ul>
+                <button type="button" className="btnLogIn" onClick={()=>setShowLogInForm(true)}>Log In</button>
+                {
+                    showLogInForm && <LogInForm closeForm={()=> setShowLogInForm(false)}/>
+                }
+            </header>
+        </div>
+    )
 };
-
-export default Header;
---
+++ src/components/form.js
@@ -0,0 +1,43 @@
+import React, {use, useState} from "react";
+
+
+export default function LogInForm({closeForm}) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [repassword, setRepassword] = useState("");
+    
+    const handleSubmit = ()=> {
+        const caseCheck = new RegExp("[A-Z]");
+
+        if (!caseCheck.test(username)) {
+            alert("Username must contain at least one uppercase letter");
+            return;
+        }
+        if (password.length < 6 || password.length > 12) {
+            alert("Password must be between 6 and 12 characters");
+            return;
+        }
+        if (password !== repassword) {
+            alert ("Please the enter correct password!");
+            return;
+        }
+        else {
+            alert("Log In Successful!");
+            closeForm();
+        }
+    };
+    return(
+        <div className="overlay">
+            <div className="LogInForm">
+                <h2>Log In</h2>
+                <form>
+                    <input type="text" placeholder="Enter the Username" value={username} onChange={(event)=>setUsername(event.target.value)}></input><br></br>
+                    <input type="password" placeholder="Enter the Password" value={password} onChange={(event)=>setPassword(event.target.value)}></input><br></br>
+                    <input type="password" placeholder="Re-password" value={repassword} onChange={(event)=>setRepassword(event.target.value)}></input><br></br><br></br>
+                    <button type="submit" onClick={handleSubmit}>Submit</button>
+                    <button type="button" onClick={closeForm}>Cancel</button>
+                </form>
+            </div>
+        </div>
+    );
+}
