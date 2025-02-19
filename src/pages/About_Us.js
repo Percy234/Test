61ba35539d14e63bcb8b1c24051b23f223c13904
ttloca24102 <ttloca24102@cusc.ctu.edu.vn>
2025-02-19 10:27:26 +0700

    about

--- src/pages/About_Us.js
+++ src/pages/About_Us.js
@@ -5,21 +5,41 @@ const AboutUs = () =>{
    return(
     <div className='about'>
         <div className='about-row'>
-            <div className='about__colum-2-4'>
-                <h3 className="about-2-5">High Quality</h3>
-                <a>We take a goal-oriented approach to healing. Whatever your goals might be we want to help you achieve them</a>
+            <div className="about-row2">
+                <div>
+                <h3 className="about-2-5">Our Philosophy</h3>
+                    <a>Physio Physical Therapy is committed to providing high quality outpatient physical therapy services in beautiful state-of-the-art facilities.
+
+                            As a healthcare leader we partner with groups and events within the communities we serve. Our focus on our physical therapists continuing education is unmatched and guarantees our physical therapists understand and put into practice cutting-edge treatments and rehabilitation techniques available for all types of injuries and conditions.</a>
+                </div>
+                <div>
+                <h3 className="about-2-5">Our Mission</h3>
+                    <a>Is to provide therapy at the highest standard of quality in private room one-on-one care, with practices in a comfortable and professional environment, and ensure that our healthcare solutions meet your individual needs.
+                    We are committed as primary healthcare professionals to help our clients resume their lifelong pursuit of health, fitness, and well-being.</a>
+                </div>
+                <div className='about__colum-2-4'>
+                    <h3 className="about-2-5">High Quality</h3>
+                    <a>Zara Therapy Clinic is a prominent healthcare facility specializing in comprehensive treatment and recovery services. With a mission to provide optimal care for each client, Zara Therapy Clinic is committed to using modern treatment methods, combining science and technology to improve health, restore function, and enhance the quality of life.We take a goal-oriented approach to healing. Whatever your goals might be we want to help you achieve them</a>
+                </div>
             </div>
-            <div className='about__colum-2-4'>
-                <h3 className="about-2-5">Patient Care</h3>
-                <a>We’ll provide hands-on treatment in the office and education you can take home with you, enabling you to become an active participant in your recovery and future physical health.</a>
+            <div className="about-row2">
+                <div>
+                    
+                </div>
             </div>
-            <div className='about__colum-2-4'>
-                <h3 className="about-2-5">Professional Service</h3>
-                <a>Our five physical therapists have a combined 120 years of clinical experience and a shared passion for helping our community stay active.</a>
-            </div>
-            <div className='about__colum-2-4'>
-                <h3 className="about-2-5">Free Consultation</h3>
-                <a>Our practitioners are very happy to give you a free consultation and guide you to the best service that can suit your medical needs. Just give us a call and book for a consultation.</a>
+            <div className="about-row2">
+                <div className='about__colum-2-4'>
+                    <h3 className="about-2-5">Patient Care</h3>
+                    <a>In addition to physical therapies, Zara Therapy Clinic also places a strong emphasis on building a healthy psychological environment for clients. Treatments aimed at reducing stress, anxiety, and improving mental well-being are scientifically and effectively applied by the clinic’s team of professionals. This not only helps clients recover their health quickly but also provides a holistic healthcare experience.We’ll provide hands-on treatment in the office and education you can take home with you, enabling you to become an active participant in your recovery and future physical health.</a>
+                </div>
+                <div className='about__colum-2-4'>
+                    <h3 className="about-2-5">Professional Service</h3>
+                    <a>Equipped with advanced devices and a team of highly qualified experts, Zara Therapy Clinic offers a wide range of services, including injury treatment, post-surgery recovery, physical therapy, relaxation massage, and health recovery programs for the elderly. The clinic always focuses on customer care, safety, and providing a comfortable, relaxing environment to ensure that clients feel confident throughout their treatment process.</a>
+                </div>
+                <div className='about__colum-2-4'>
+                    <h3 className="about-2-5">Free Consultation</h3>
+                    <a>Zara Therapy Clinic encourages clients to maintain a regular health care routine to prevent and treat both physical and mental issues. With a dedicated team of specialists, Zara Therapy Clinic is the ideal choice for those looking to regain their health and balance in life.Our practitioners are very happy to give you a free consultation and guide you to the best service that can suit your medical needs. Just give us a call and book for a consultation.</a>
+                </div>
             </div>
         </div>
     </div>
