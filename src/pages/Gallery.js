f8c6380b9ef3b081fb278514f034dd08ceb5c27a
ttloca24102 <ttloca24102@cusc.ctu.edu.vn>
2025-02-27 15:49:58 +0700

    thêm ảnh vào gallery với therapist

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -2,12 +2,10 @@ import React from 'react';
 import '../css/gallery.css';
 
 const images = [
-    { src: '/img/gallery/image1.jpg', alt: 'Image 1' },
-    { src: '/img/gallery/image2.jpg', alt: 'Image 2' },
-    { src: '/img/gallery/image3.jpg', alt: 'Image 3' },
-    { src: '/img/gallery/image4.jpg', alt: 'Image 4' },
-    { src: '/img/gallery/image5.jpg', alt: 'Image 5' },
-    { src: '/img/gallery/image6.jpg', alt: 'Image 6' },
+    { src: '/img/gallery/gallery1.png', alt: 'Image 1' },
+    { src: '/img/gallery/gallery2.png', alt: 'Image 2' },
+    { src: '/img/gallery/gallery3.png', alt: 'Image 3' },
+    { src: '/img/gallery/gallery4.png', alt: 'Image 4' },
 ];
 
 export default function Gallery() {
@@ -21,6 +19,11 @@ export default function Gallery() {
                     </div>
                 ))}
             </div>
+            <p>
+                Zara Therapy Clinic is a specialized area in medical facilities, equipped with equipment and treatment methods to support patients in rehabilitation after trauma, surgery or diseases affecting the motor system. 
+                Here, patients will be guided by physical therapists to perform exercises, using support equipment such as spinal traction machines, therapeutic ultrasound, electrotherapy or thermotherapy to relieve pain, improve muscle strength and increase mobility. 
+                The physical therapy room plays an important role in the recovery process, helping patients regain normal activities and improve their quality of life.
+            </p>
         </div>
     );
 }
